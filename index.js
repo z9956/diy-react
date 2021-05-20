207061da55a8069f360aa87c6750275fd8b47c81
9956 <zhz995617@gmail.com>
2021-05-21 00:17:05 +0800

    style: style

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
-const CHILDREN = 'children'
-export const TEXT_ELEMENT = 'TEXT ELEMENT'
+const CHILDREN = 'children';
+export const TEXT_ELEMENT = 'TEXT ELEMENT';
 
 export const render = (element, parentDom) => {
-  const { type, props } = element
+	const { type, props } = element;
 
-  const isTextElement = type === TEXT_ELEMENT
+	const isTextElement = type === TEXT_ELEMENT;
 
-  const dom = isTextElement ? document.createTextNode('') : document.createElement(type)
+	const dom = isTextElement
+		? document.createTextNode('')
+		: document.createElement(type);
 
-  const isListener = name => name.startsWith('on')
+	const isListener = (name) => name.startsWith('on');
 
-  const keysProps = Object.keys(props)
+	const keysProps = Object.keys(props);
 
-  keysProps.filter(isListener).forEach(name => {
-    const eventType = name.toLowerCase().substring(2)
-    dom.addEventListener(eventType, props[name])
-  })
+	keysProps.filter(isListener).forEach((name) => {
+		const eventType = name.toLowerCase().substring(2);
+		dom.addEventListener(eventType, props[name]);
+	});
 
-  const isAttribute = name => !isListener(name) && name !== CHILDREN
-  keysProps.filter(isAttribute).forEach(name => {
-    dom[name] = props[name]
-  })
+	const isAttribute = (name) => !isListener(name) && name !== CHILDREN;
+	keysProps.filter(isAttribute).forEach((name) => {
+		dom[name] = props[name];
+	});
 
-  const childElements = props.children ?? []
-  childElements.forEach(childElement => render(childElement, dom))
+	const childElements = props.children ?? [];
+	childElements.forEach((childElement) => render(childElement, dom));
 
-  parentDom.appendChild(dom)
-}
-
-// export { render };
+	parentDom.appendChild(dom);
+};
