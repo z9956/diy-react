82cb61d6a0d4c79d893a127a690a59e15078b446
9956 <zhz995617@gmail.com>
2021-06-22 23:20:39 +0800

    feat: 函数组件、Hooks

--- src/didact.js
+++ src/didact.js
@@ -5,6 +5,14 @@ const UPDATE = 'UPDATE';
 const DELETION = 'DELETION';
 const PLACEMENT = 'PLACEMENT';
 
+let currentRoot = null;
+let nextUnitOfWork = null;
+let wipRoot = null;
+let deletions = null;
+
+let wipFiber = null;
+let hookIndex = null;
+
 const isEvent = (key) => key.startsWith('on');
 const isProperty = (key) => key !== CHILDREN && !isEvent(key);
 const isNew = (prev, next) => (key) => prev[key] !== next[key];
@@ -55,11 +63,6 @@ const render = (element, container) => {
 	nextUnitOfWork = wipRoot;
 };
 
-let currentRoot = null;
-let nextUnitOfWork = null;
-let wipRoot = null;
-let deletions = null;
-
 const workLoop = (deadline) => {
 	let shouldYield = false;
 
@@ -85,18 +88,32 @@ const commitRoot = () => {
 const commitWork = (fiber) => {
 	if (!fiber) return;
 
-	const domParent = fiber.parent.dom;
+	let domParentFiber = fiber.parent;
+
+	while (!domParentFiber.dom) {
+		domParentFiber = domParentFiber.parent;
+	}
+
+	const domParent = domParentFiber.dom;
 
 	if (fiber.effectTag === PLACEMENT && fiber.dom != null) {
 		domParent.appendChild(fiber.dom);
 	} else if (fiber.effectTag === UPDATE && fiber.dom != null) {
 		updateDom(fiber.dom, fiber.alternate.props, fiber.props);
 	} else if (fiber.effectTag === DELETION) {
-		domParent.removeChild(fiber.dom);
+		commitDeletion(fiber, domParent);
 	}
 
 	commitWork(fiber.child);
 	commitWork(fiber.sibling);
+};
+
+const commitDeletion = (fiber, domParent) => {
+	if (fiber.dom) {
+		domParent.removeChild(fiber.dom);
+	} else {
+		commitDeletion(fiber.child, domParent);
+	}
 };
 
 const updateDom = (dom, prevProps, nextProps) => {
@@ -134,12 +151,13 @@ const updateDom = (dom, prevProps, nextProps) => {
 };
 
 const performUnitOfWork = (fiber) => {
-	if (!fiber.dom) {
-		fiber.dom = createDom(fiber);
-	}
+	const isFunctionComponent = fiber.type instanceof Function;
 
-	const elements = fiber.props.children;
-	reconcileChildren(fiber, elements);
+	if (isFunctionComponent) {
+		updateFunctionComponent(fiber);
+	} else {
+		updateHostComponent(fiber);
+	}
 
 	if (fiber.child) {
 		return fiber.child;
@@ -207,4 +225,52 @@ const reconcileChildren = (wipFiber, elements) => {
 	}
 };
 
-export { render, createElement };
+const updateHostComponent = (fiber) => {
+	if (!fiber.dom) fiber.dom = createDom(fiber);
+	reconcileChildren(fiber, fiber.props.children);
+};
+
+const updateFunctionComponent = (fiber) => {
+	wipFiber = fiber;
+	hookIndex = 0;
+	wipFiber.hooks = [];
+
+	const children = [fiber.type(fiber.props)];
+	reconcileChildren(fiber, children);
+};
+
+const useState = (initial) => {
+	const oldHook =
+		wipFiber.alternate &&
+		wipFiber.alternate.hooks &&
+		wipFiber.alternate.hooks[hookIndex];
+
+	const hook = {
+		state: oldHook ? oldHook.state : initial,
+		queue: [],
+	};
+
+	const actions = oldHook ? oldHook.queue : [];
+	actions.forEach((action) => {
+		hook.state = action(hook.state);
+	});
+
+	const setState = (action) => {
+		hook.queue.push(action);
+		wipRoot = {
+			dom: currentRoot.dom,
+			props: currentRoot.props,
+			alternate: currentRoot,
+		};
+
+		nextUnitOfWork = wipRoot;
+		deletions = [];
+	};
+
+	wipFiber.hooks.push(hook);
+	hookIndex++;
+
+	return [hook.state, setState];
+};
+
+export { render, createElement, useState };
--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,27 @@
-import { render, createElement } from './didact.js';
+import { render, useState } from './didact';
 /** @jsx createElement */
 
-const updateValue = (e) => {
-	rerender(e.target.value);
+const Header = (props) => {
+	return <h1>{props.title}</h1>;
+};
+
+const App = () => {
+	const [state, setState] = useState(1);
+
+	const handleAdd = () => {
+		setState((count) => count + 1);
+	};
+
+	return (
+		<div>
+			<Header title="title" />
+			<button onClick={handleAdd}>add</button>
+			<p>
+				count:<span>{state}</span>
+			</p>
+		</div>
+	);
 };
 
 const container = document.getElementById('root');
-const rerender = (value) => {
-	const element = (
-		<div>
-			<input onInput={updateValue} value={value} />
-			<h2>Hello {value}</h2>
-		</div>
-	);
-
-	render(element, container);
-};
-
-rerender('world');
+render(<App />, container);
